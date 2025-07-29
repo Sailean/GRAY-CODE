023a9a38341c7a6a2282eb3cf779d6da25473e5a
Sailean514 <134035079+Sailean@users.noreply.github.com>
2025-07-29 22:42:10 +0900

    Add files via upload

---
+++ src/server.py
@@ -0,0 +1,25 @@
+from http.server import SimpleHTTPRequestHandler, HTTPServer
+
+class CustomHandler(SimpleHTTPRequestHandler):
+    def end_headers(self):
+        if self.path.endswith(".json"):
+            self.send_header("Content-Type", "application/json")
+        super().end_headers()
+
+PORT = 5500
+with HTTPServer(("localhost", PORT), CustomHandler) as httpd:
+    print(f"Serving on http://localhost:{PORT}")
+    httpd.serve_forever()
+=======
+from http.server import SimpleHTTPRequestHandler, HTTPServer
+
+class CustomHandler(SimpleHTTPRequestHandler):
+    def end_headers(self):
+        if self.path.endswith(".json"):
+            self.send_header("Content-Type", "application/json")
+        super().end_headers()
+
+PORT = 5500
+with HTTPServer(("localhost", PORT), CustomHandler) as httpd:
+    print(f"Serving on http://localhost:{PORT}")
+    httpd.serve_forever()
